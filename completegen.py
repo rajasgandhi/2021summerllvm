46de0ddecdaa3207e691feba3f0b4c8e9c0b0992
rajasgandhi <rajasmgandhi@gmail.com>
2021-07-24 10:25:50 -0500

    started to create a complete start to end finish python generator file

---
+++ completegen.py
@@ -0,0 +1,9 @@
+import os
+import subprocess
+
+list_files = subprocess.run(["clang -Xclang -disable-O0-optnone -S -emit-llvm input.c -o input.ll",
+ "cd build", 
+    "make", 
+    "cd ..",
+    "opt -enable-new-pm=0 -load build/skeleton/libSkeletonPass.so -hello1 -S input.ll -o output.ll",
+    "python3 transformjson.py"], shell=True) 
