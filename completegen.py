9fd6f17ec1e8a94458f039d086fb96585db90f05
rajasgandhi <rajasmgandhi@gmail.com>
2021-08-02 16:37:17 -0500

    update

--- completegen.py
+++ completegen.py
@@ -3,9 +3,13 @@ import subprocess
 import sys
 
 compileCFile = "clang -Xclang -disable-O0-optnone -S -emit-llvm " + sys.argv[1] + " -o input.ll"
-#print(compileCFile)
+file = os.getcwd() + "/outputtransformed.json"
+copyJSON = "cp " + file + " " + sys.argv[2]
+#print(copyJSON)
 
 subprocess.run([compileCFile], shell=True)
 subprocess.run(["make"], cwd="build/", shell=True)
 subprocess.run(["opt -enable-new-pm=0 -load build/skeleton/libSkeletonPass.so -hello1 -S input.ll -o output.ll"], shell=True)
 subprocess.run(["python3 transformjson.py"], shell=True)
+subprocess.run([copyJSON], shell=True)
+subprocess.run(["npm start"], cwd="/home/rajas/Documents/GitHub/2021summervistool", shell=True)
